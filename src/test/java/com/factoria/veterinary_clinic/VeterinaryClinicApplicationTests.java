a31449b64fab11bf680e0987ef76da7ccc96c2af
NepyAnna <nepyann@gmail.com>
2024-11-25 15:43:38 +0100

    add branch dev

---
+++ src/test/java/com/factoria/veterinary_clinic/VeterinaryClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factoria.veterinary_clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinaryClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
