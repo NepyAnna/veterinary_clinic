8e18e3aceace6b0591785e19c07fb4aafd1f5f84
NepyAnna <nepyann@gmail.com>
2024-11-28 19:10:40 +0100

    add class Appointment

--- src/main/java/com/factoria/veterinary_clinic/models/Appointment.java
+++ src/main/java/com/factoria/veterinary_clinic/models/Appointment.java
@@ -1,5 +1,39 @@
 package com.factoria.veterinary_clinic.models;
 
+import java.time.LocalDateTime;
+
 public class Appointment {
-    
+    private Long id;
+    private LocalDateTime appointmentDateTime;
+    private String type;
+    private String reason;
+    private String status;
+
+    protected Appointment() {}
+
+    public Appointment(Long id, LocalDateTime appointmentDateTime,String type, String reason, String status) {
+        this.type = type;
+        this.reason = reason;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDateTime getAppointmentDateTime() {
+        return appointmentDateTime;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 }
