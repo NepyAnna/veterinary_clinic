50e044b859ed3ede019f6744c3b923c98d251974
NepyAnna <nepyann@gmail.com>
2024-12-01 21:23:06 +0100

    rebase branch to dev

--- src/main/java/com/factoria/veterinary_clinic/services/AppointmentService.java
+++ src/main/java/com/factoria/veterinary_clinic/services/AppointmentService.java
@@ -1,5 +1,21 @@
 package com.factoria.veterinary_clinic.services;
 
-public class AppointmentService {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.factoria.veterinary_clinic.models.Appointment;
+import com.factoria.veterinary_clinic.repositories.AppointmentRepository;
+
+@Service
+public class AppointmentService {
+    private AppointmentRepository repository;
+
+    public AppointmentService(AppointmentRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Appointment> findAll(){
+        return repository.findAll();
+    }
 }
