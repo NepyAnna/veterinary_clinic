2c2073588c6830f7e3179af4a755b97d9b75f885
NepyAnna <nepyann@gmail.com>
2024-12-02 21:13:57 +0100

    add createAppointment and updateAppointment to service

--- src/main/java/com/factoria/veterinary_clinic/controllers/AppointmentController.java
+++ src/main/java/com/factoria/veterinary_clinic/controllers/AppointmentController.java
@@ -3,10 +3,8 @@ package com.factoria.veterinary_clinic.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.factoria.veterinary_clinic.dtos.AppointmentDto;
-import com.factoria.veterinary_clinic.models.Appointment;
 import com.factoria.veterinary_clinic.services.AppointmentService;
 import java.util.List;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,9 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
 @RestController
-@RequestMapping(path="appointments")
+@RequestMapping(path = "appointments")
 public class AppointmentController {
     private AppointmentService service;
 
@@ -27,27 +24,28 @@ public class AppointmentController {
     @GetMapping("")
     public List<AppointmentDto> index() {
         return service.findAll().stream()
-        .map(appointment -> new AppointmentDto(
-                          appointment.getPatient() != null ? appointment.getPatient().getId() : null,
-                          appointment.getPatient() != null ? appointment.getPatient().getName() : null,
-                          appointment.getAppointmentDateTime(),
-                          appointment.getType(),
-                          appointment.getReason(),
-                          appointment.getStatus()
-                      ))
-                      .toList();
+                .map(appointment -> new AppointmentDto(
+                        appointment.getPatient() != null ? appointment.getPatient().getId() : null,
+                        appointment.getPatient() != null ? appointment.getPatient().getName() : null,
+                        appointment.getAppointmentDateTime(),
+                        appointment.getType(),
+                        appointment.getReason(),
+                        appointment.getStatus()))
+                .toList();
     }
 
     @PostMapping("")
-    public AppointmentDto store(@RequestBody AppointmentDto entity) {///Or maybe metod type shoud be Appointment??
-        return entity;
-    } 
+    public AppointmentDto store(@RequestBody AppointmentDto newAppointment) {
+        return service.createAppointment(newAppointment);
+    }
 
     @PutMapping("/{id}")
-    public Appointment updateAppointment(@PathVariable Long id, @RequestBody Appointment appointment) {
-        return service.updateAppointment(id, appointment);
+    public AppointmentDto updateAppointment(
+            @PathVariable Long id,
+            @RequestBody AppointmentDto updatedData) {
+        return service.updateAppointment(id, updatedData); 
     }
-    
+
     @DeleteMapping("/{id}")
     public void deleteAppointment(@PathVariable Long id) {
         service.deleteAppointment(id);
--- src/main/java/com/factoria/veterinary_clinic/repositories/AppointmentRepository.java
+++ src/main/java/com/factoria/veterinary_clinic/repositories/AppointmentRepository.java
@@ -1,6 +1,15 @@
 package com.factoria.veterinary_clinic.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.factoria.veterinary_clinic.models.Appointment;
+import java.util.Optional;
 
-public interface AppointmentRepository extends JpaRepository<Appointment, Long> {}
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.factoria.veterinary_clinic.models.Appointment;
+import com.factoria.veterinary_clinic.models.Patient;
+
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+     @Query("SELECT p FROM Patient p WHERE p.id = :id")
+    Optional<Patient> findPatientById(@Param("id") Long id);
+}
