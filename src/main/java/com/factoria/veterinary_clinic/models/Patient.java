a13b46578e1a3fe7e4faf4222f3c1bfe63a3a821
AnaTovar-Arg <tovar12kari@gmail.com>
2024-12-04 14:41:25 +0100

    changes in userdto and solved in dev

--- src/main/java/com/factoria/veterinary_clinic/models/Patient.java
+++ src/main/java/com/factoria/veterinary_clinic/models/Patient.java
@@ -3,7 +3,21 @@ package com.factoria.veterinary_clinic.models;
 import java.util.Date;
 import java.util.List;
 
-import jakarta.persistence.*;
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 
 @Entity
 @Table(name = "patients")
