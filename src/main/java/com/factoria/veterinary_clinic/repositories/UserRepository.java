7e1f6ef5d43574a8b9a60d5362adc32f11b3cd16
tizzifona <tizzifona@gmail.com>
2024-12-08 11:21:49 +0100

    Added code for registration and authentication

--- src/main/java/com/factoria/veterinary_clinic/repositories/UserRepository.java
+++ src/main/java/com/factoria/veterinary_clinic/repositories/UserRepository.java
@@ -1,6 +1,11 @@
 package com.factoria.veterinary_clinic.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.factoria.veterinary_clinic.models.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
